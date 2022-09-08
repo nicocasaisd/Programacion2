aac92d62b0493aa76482f0c33a55cc3c2adf1aac
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-09-08 20:38:20 -0300

    Ejercicio 02 Windows Forms

--- C05b_Ejercicio02/Form1.Designer.cs
+++ C05b_Ejercicio02/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpDatos = new System.Windows.Forms.GroupBox();
+            this.grpGenero = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // grpDatos
+            // 
+            this.grpDatos.Location = new System.Drawing.Point(27, 24);
+            this.grpDatos.Name = "grpDatos";
+            this.grpDatos.Size = new System.Drawing.Size(212, 127);
+            this.grpDatos.TabIndex = 0;
+            this.grpDatos.TabStop = false;
+            this.grpDatos.Text = "Detalles de usuario";
+            // 
+            // grpGenero
+            // 
+            this.grpGenero.Location = new System.Drawing.Point(27, 172);
+            this.grpGenero.Name = "grpGenero";
+            this.grpGenero.Size = new System.Drawing.Size(212, 100);
+            this.grpGenero.TabIndex = 1;
+            this.grpGenero.TabStop = false;
+            this.grpGenero.Text = "groupBox1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(368, 315);
+            this.Controls.Add(this.grpGenero);
+            this.Controls.Add(this.grpDatos);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,5 +64,8 @@
         }
 
         #endregion
+
+        private GroupBox grpDatos;
+        private GroupBox grpGenero;
     }
 }
