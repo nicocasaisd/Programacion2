8807e0786b810a8e750116dc7d2fc8e52fd829f8
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-09-15 11:16:37 -0300

    C06_E02

---
+++ C06_E02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C06_E02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LISTA
+
+            // Creo la lista
+            List<int> lista = new List<int>();
+            // Cargo con Random
+            Random random = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                lista.Add(random.Next(-20, 20));
+            }
+            // Muestro
+            Console.WriteLine("Array original.");
+            foreach(int i in lista)
+            {
+                Console.WriteLine($"Elemento: {i}");
+            }
+
+        }
+    }
+}
