bac93006b6f1b5fec4047ac6ae16e672fce6c146
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-10-20 18:55:48 -0300

    Herencia - I02

---
+++ C08/C02_Go_Speed_Racer/C02_Go_Speed_Racer/Biblioteca/Competencia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Biblioteca
+{
+    public class Competencia
+    {
+        private short cantidadCompetidores;
+        private short cantidadVueltas;
+        private List<VehiculoDeCarrera> competidores;
+        private TipoCompetencia tipo;
+
+        public short CantidadCompetidores
+        {
+            get { return cantidadCompetidores; }
+            set { cantidadCompetidores = value; }
+        }
+
+        public short CantidadVueltas 
+        { 
+            get {return cantidadVueltas; } 
+            set {cantidadVueltas = value; }
+        }
+
+    }
+
+    public enum TipoCompetencia
+    {
+        F1, MotoCross
+    }
+}
