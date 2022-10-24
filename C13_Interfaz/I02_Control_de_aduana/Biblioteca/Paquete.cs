cae418f4cc20114b5876023e961d5eccd737d9f7
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-10-24 19:13:04 -0300

    Interfaz - E02

---
+++ C13_Interfaz/I02_Control_de_aduana/Biblioteca/Paquete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public abstract class Paquete
+    {
+        #region ATRIBUTOS
+
+        string codigoSeguimiento;
+        protected decimal costoEnvio;
+        string destino;
+        string origen;
+        double pesoKg;
+
+        #endregion
+
+        #region PROPIEDADES
+        public abstract bool TienePrioridad
+        {
+            get;
+        }
+
+        #endregion 
+    }
+}
