9be358a9b2ee41c3ee995590787ba295760711aa
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-11-07 14:20:25 -0300

    forms

---
+++ C17_SQL/I01_Mi_primer_CRUD/Vistas/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Vistas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Nombre = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_Guardar = new System.Windows.Forms.Button();
+            this.txt_Apellido = new System.Windows.Forms.TextBox();
+            this.lbl_Nombre = new System.Windows.Forms.Label();
+            this.lbl_Apellido = new System.Windows.Forms.Label();
+            this.btn_Modificar = new System.Windows.Forms.Button();
+            this.btn_Eliminar = new System.Windows.Forms.Button();
+            this.btn_Leer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Location = new System.Drawing.Point(104, 38);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(120, 23);
+            this.txt_Nombre.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(18, 123);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(206, 154);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.Location = new System.Drawing.Point(18, 314);
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Guardar.TabIndex = 2;
+            this.btn_Guardar.Text = "Guardar";
+            this.btn_Guardar.UseVisualStyleBackColor = true;
+            // 
+            // txt_Apellido
+            // 
+            this.txt_Apellido.Location = new System.Drawing.Point(104, 79);
+            this.txt_Apellido.Name = "txt_Apellido";
+            this.txt_Apellido.Size = new System.Drawing.Size(120, 23);
+            this.txt_Apellido.TabIndex = 3;
+            // 
+            // lbl_Nombre
+            // 
+            this.lbl_Nombre.AutoSize = true;
+            this.lbl_Nombre.Location = new System.Drawing.Point(18, 38);
+            this.lbl_Nombre.Name = "lbl_Nombre";
+            this.lbl_Nombre.Size = new System.Drawing.Size(54, 15);
+            this.lbl_Nombre.TabIndex = 4;
+            this.lbl_Nombre.Text = "Nombre:";
+            // 
+            // lbl_Apellido
+            // 
+            this.lbl_Apellido.AutoSize = true;
+            this.lbl_Apellido.Location = new System.Drawing.Point(18, 79);
+            this.lbl_Apellido.Name = "lbl_Apellido";
+            this.lbl_Apellido.Size = new System.Drawing.Size(54, 15);
+            this.lbl_Apellido.TabIndex = 5;
+            this.lbl_Apellido.Text = "Apellido:";
+            // 
+            // btn_Modificar
+            // 
+            this.btn_Modificar.Location = new System.Drawing.Point(104, 314);
+            this.btn_Modificar.Name = "btn_Modificar";
+            this.btn_Modificar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Modificar.TabIndex = 6;
+            this.btn_Modificar.Text = "Modificar";
+            this.btn_Modificar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Eliminar
+            // 
+            this.btn_Eliminar.Location = new System.Drawing.Point(185, 314);
+            this.btn_Eliminar.Name = "btn_Eliminar";
+            this.btn_Eliminar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Eliminar.TabIndex = 7;
+            this.btn_Eliminar.Text = "Eliminar";
+            this.btn_Eliminar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Leer
+            // 
+            this.btn_Leer.Location = new System.Drawing.Point(266, 314);
+            this.btn_Leer.Name = "btn_Leer";
+            this.btn_Leer.Size = new System.Drawing.Size(75, 23);
+            this.btn_Leer.TabIndex = 8;
+            this.btn_Leer.Text = "Leer";
+            this.btn_Leer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 374);
+            this.Controls.Add(this.btn_Leer);
+            this.Controls.Add(this.btn_Eliminar);
+            this.Controls.Add(this.btn_Modificar);
+            this.Controls.Add(this.lbl_Apellido);
+            this.Controls.Add(this.lbl_Nombre);
+            this.Controls.Add(this.txt_Apellido);
+            this.Controls.Add(this.btn_Guardar);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txt_Nombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Nombre;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_Guardar;
+        private System.Windows.Forms.TextBox txt_Apellido;
+        private System.Windows.Forms.Label lbl_Nombre;
+        private System.Windows.Forms.Label lbl_Apellido;
+        private System.Windows.Forms.Button btn_Modificar;
+        private System.Windows.Forms.Button btn_Eliminar;
+        private System.Windows.Forms.Button btn_Leer;
+    }
+}
