1c3c396c30deacbddcbbcd8ff7bb956eb3e26984
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-11-10 20:25:25 -0300

    C19_Multithreading

---
+++ C19_Multithreading/Explicacion/Consola/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(
+                () =>
+                {
+                    Console.WriteLine("Inicia task 1");
+                    try
+                    {
+                        throw new Exception("Desde el hilo 1 tiro la excepcion");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                );
+            Task t2 = new Task(() =>
+            {
+                Console.WriteLine("Inicia task 2");
+                //throw new Exception("Desde adentro del hilo 2");
+                Console.WriteLine("Mensaje desde el hilo 2");
+            });
+
+            t1.Start();
+            try
+            {
+                t2.Start();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error atrapado en el try externo");
+            }
+
+            Task.WaitAll(t1,t2);
+            Console.WriteLine("Fin del programa");
+        }
+
+        //public delegate void DelegadoAccion();
+
+            
+
+    }
+}
---
+++ C19_Multithreading/I01_El_relojero/Vista/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void AsignarHora()
+        {
+            DateTime ahora = DateTime.Now;
+            this.lblHora.Text = $"{ahora.Hour}:{ahora.Minute}:{ahora.Second}";
+            //Thread.Sleep(1000);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.Show();
+            AsignarHora();
+            //while (true)
+            //{
+            //    AsignarHora();
+
+            //}
+            //MessageBox.Show("Terminado");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            AsignarHora();
+        }
+    }
+}
