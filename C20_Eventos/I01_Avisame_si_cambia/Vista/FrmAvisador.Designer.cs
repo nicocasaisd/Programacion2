fc084664293eb6c438fd0d3427faa43d0ea407bb
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-11-16 20:30:59 -0300

    cambio de lugar las carpetas

---
+++ C20_Eventos/I01_Avisame_si_cambia/Vista/FrmAvisador.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Vista
+{
+    partial class FrmAvisador
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Crear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_Nombre = new System.Windows.Forms.Label();
+            this.lbl_Apellido = new System.Windows.Forms.Label();
+            this.lbl_NombreCompleto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Crear
+            // 
+            this.btn_Crear.Location = new System.Drawing.Point(271, 212);
+            this.btn_Crear.Name = "btn_Crear";
+            this.btn_Crear.Size = new System.Drawing.Size(75, 23);
+            this.btn_Crear.TabIndex = 0;
+            this.btn_Crear.Text = "button1";
+            this.btn_Crear.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lbl_Nombre
+            // 
+            this.lbl_Nombre.AutoSize = true;
+            this.lbl_Nombre.Location = new System.Drawing.Point(82, 62);
+            this.lbl_Nombre.Name = "lbl_Nombre";
+            this.lbl_Nombre.Size = new System.Drawing.Size(38, 15);
+            this.lbl_Nombre.TabIndex = 2;
+            this.lbl_Nombre.Text = "label1";
+            // 
+            // lbl_Apellido
+            // 
+            this.lbl_Apellido.AutoSize = true;
+            this.lbl_Apellido.Location = new System.Drawing.Point(152, 166);
+            this.lbl_Apellido.Name = "lbl_Apellido";
+            this.lbl_Apellido.Size = new System.Drawing.Size(38, 15);
+            this.lbl_Apellido.TabIndex = 3;
+            this.lbl_Apellido.Text = "label1";
+            // 
+            // lbl_NombreCompleto
+            // 
+            this.lbl_NombreCompleto.AutoSize = true;
+            this.lbl_NombreCompleto.Font = new System.Drawing.Font("Segoe UI", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_NombreCompleto.Location = new System.Drawing.Point(94, 268);
+            this.lbl_NombreCompleto.Name = "lbl_NombreCompleto";
+            this.lbl_NombreCompleto.Size = new System.Drawing.Size(112, 47);
+            this.lbl_NombreCompleto.TabIndex = 4;
+            this.lbl_NombreCompleto.Text = "label1";
+            // 
+            // FrmAvisador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 346);
+            this.Controls.Add(this.lbl_NombreCompleto);
+            this.Controls.Add(this.lbl_Apellido);
+            this.Controls.Add(this.lbl_Nombre);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Crear);
+            this.Name = "FrmAvisador";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Crear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_Nombre;
+        private System.Windows.Forms.Label lbl_Apellido;
+        private System.Windows.Forms.Label lbl_NombreCompleto;
+    }
+}
