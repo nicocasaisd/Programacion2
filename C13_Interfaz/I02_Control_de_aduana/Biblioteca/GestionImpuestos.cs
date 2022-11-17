7135a92fd59e232597340b68f7e3956eaccd8a17
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-11-17 12:24:06 -0300

    terminado ejercicio Gestion de impuestos (sin unit testing)

---
+++ C13_Interfaz/I02_Control_de_aduana/Biblioteca/GestionImpuestos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class GestionImpuestos
+    {
+        List<IAduana> impuestosAduana;
+        List<IAfip> impuestosAfip;
+
+        public GestionImpuestos()
+        {
+            this.impuestosAduana = new List<IAduana>();
+            this.impuestosAfip = new List<IAfip>();
+        }
+
+        public decimal CalcularTotalImpuestosAduana()
+        {
+            decimal totalImpuestosAduana = 0;
+
+            foreach(IAduana paquete in impuestosAduana)
+            {
+                totalImpuestosAduana += paquete.Impuestos;
+            }
+            return totalImpuestosAduana;
+        }
+
+
+        public decimal CalcularTotalImpuestosAfip()
+        {
+            decimal totalImpuestosAfip = 0;
+
+            foreach(IAfip paquete in impuestosAfip)
+            {
+                totalImpuestosAfip += ((IAfip)paquete).Impuestos;
+            }
+            return totalImpuestosAfip;
+        }
+
+        public void RegistrarImpuestos(IEnumerable<Paquete> paquetes)
+        {
+            foreach (Paquete item in paquetes)
+            {
+                RegistrarImpuestos(item);
+            }
+        }
+
+        public void RegistrarImpuestos(Paquete paquete)
+        {
+            this.impuestosAduana.Add(paquete);
+            if(paquete is IAfip)
+            {
+                this.impuestosAfip.Add((IAfip)paquete);
+            }
+        }
+
+
+
+    }
+}
